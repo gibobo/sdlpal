9f3620ad7a33cbd3bd4f8030743594b56e95fece
gibobo <gibobo831521.dc@gmail.com>
2024-08-27 18:04:00 +0800

    wip240827-5

--- win32/pal_config.h
+++ win32/pal_config.h
@@ -24,35 +24,35 @@
 
 #pragma once
 
-#define PAL_PREFIX            "E:/Pal98rqptw/"
-#define PAL_SAVE_PREFIX       "E:/Pal98rqptw/"
+#define PAL_PREFIX "C:/Users/23021/Downloads/Pal98rqptw/"
+#define PAL_SAVE_PREFIX "C:/Users/23021/Downloads/Pal98rqptw/"
 
-#define PAL_DEFAULT_WINDOW_WIDTH   640
-#define PAL_DEFAULT_WINDOW_HEIGHT  400
+#define PAL_DEFAULT_WINDOW_WIDTH 640
+#define PAL_DEFAULT_WINDOW_HEIGHT 400
 #define PAL_DEFAULT_FULLSCREEN_HEIGHT 480
 
-#if SDL_VERSION_ATLEAST(2,0,0)
-# define PAL_VIDEO_INIT_FLAGS  (SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | (gConfig.fFullScreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0))
+#if SDL_VERSION_ATLEAST(2, 0, 0)
+#define PAL_VIDEO_INIT_FLAGS (SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | (gConfig.fFullScreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0))
 #else
-# define PAL_VIDEO_INIT_FLAGS  (SDL_HWSURFACE | SDL_RESIZABLE | (gConfig.fFullScreen ? SDL_FULLSCREEN : 0))
-# define PAL_FATAL_OUTPUT(s)   MessageBoxA(0, (s), "FATAL ERROR", MB_ICONERROR)
-# define PAL_HAS_SDLCD         1
+#define PAL_VIDEO_INIT_FLAGS (SDL_HWSURFACE | SDL_RESIZABLE | (gConfig.fFullScreen ? SDL_FULLSCREEN : 0))
+#define PAL_FATAL_OUTPUT(s) MessageBoxA(0, (s), "FATAL ERROR", MB_ICONERROR)
+#define PAL_HAS_SDLCD 1
 #endif
 
 #ifdef _M_ARM
-# define PAL_SDL_INIT_FLAGS	(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_CDROM | SDL_INIT_NOPARACHUTE)
+#define PAL_SDL_INIT_FLAGS (SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_CDROM | SDL_INIT_NOPARACHUTE)
 #else
-# ifndef PAL_HAS_JOYSTICKS
-#  define PAL_HAS_JOYSTICKS    1
-# endif
-# define PAL_SDL_INIT_FLAGS	(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_CDROM | SDL_INIT_NOPARACHUTE | SDL_INIT_JOYSTICK)
+#ifndef PAL_HAS_JOYSTICKS
+#define PAL_HAS_JOYSTICKS 1
+#endif
+#define PAL_SDL_INIT_FLAGS (SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_CDROM | SDL_INIT_NOPARACHUTE | SDL_INIT_JOYSTICK)
 #endif
 
-#define PAL_PLATFORM         NULL
-#define PAL_CREDIT           NULL
-#define PAL_PORTYEAR         NULL
+#define PAL_PLATFORM NULL
+#define PAL_CREDIT NULL
+#define PAL_PORTYEAR NULL
 
-#define PAL_HAS_NATIVEMIDI  1
+#define PAL_HAS_NATIVEMIDI 1
 
 // #define PAL_HAS_CONFIG_PAGE 1
 
@@ -66,13 +66,13 @@
 #define strtok_r strtok_s
 #define strcasecmp _stricmp
 #define strncasecmp _strnicmp
-#define strdup _strdup //https://msdn.microsoft.com/en-us/library/ms235454(v=vs.140).aspx 
+#define strdup _strdup //https://msdn.microsoft.com/en-us/library/ms235454(v=vs.140).aspx
 #endif
 
-PAL_C_LINKAGE char* strcasestr(const char *, const char *);
+PAL_C_LINKAGE char *strcasestr(const char *, const char *);
 #define PAL_NEED_STRCASESTR 1
 
-#if defined (_M_ARM64) || defined(_M_ARM)
+#if defined(_M_ARM64) || defined(_M_ARM)
 #define PAL_HAS_GLSL 0
 #else
 #define PAL_HAS_GLSL 1
