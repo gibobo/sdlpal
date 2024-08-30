b6599076b5827fa80540e4f4f9b79bc4122c96ec
Gibobo Chen <gibobo831521.dc@gmail.com>
2024-08-30 20:25:00 +0800

    wip-240830

--- cmake/sdlpal.cmake
+++ cmake/sdlpal.cmake
@@ -1,36 +1,36 @@
 set(SDLPAL_SOURCE_FILES
-    ${SDLPAL_SOURCE_DIR}/audio.c
-    ${SDLPAL_SOURCE_DIR}/battle.c
-    ${SDLPAL_SOURCE_DIR}/ending.c
-    ${SDLPAL_SOURCE_DIR}/fight.c
-    ${SDLPAL_SOURCE_DIR}/font.c
-    ${SDLPAL_SOURCE_DIR}/game.c
-    ${SDLPAL_SOURCE_DIR}/global.c
-    ${SDLPAL_SOURCE_DIR}/glslp.c
-    ${SDLPAL_SOURCE_DIR}/input.c
-    ${SDLPAL_SOURCE_DIR}/itemmenu.c
-    ${SDLPAL_SOURCE_DIR}/magicmenu.c
-    ${SDLPAL_SOURCE_DIR}/map.c
-    ${SDLPAL_SOURCE_DIR}/mini_glloader.c
-    ${SDLPAL_SOURCE_DIR}/palcfg.c
-    ${SDLPAL_SOURCE_DIR}/palcommon.c
-    ${SDLPAL_SOURCE_DIR}/palette.c
-    ${SDLPAL_SOURCE_DIR}/play.c
-    ${SDLPAL_SOURCE_DIR}/res.c
-    ${SDLPAL_SOURCE_DIR}/resampler.c
-    ${SDLPAL_SOURCE_DIR}/rixplay.cpp
-    ${SDLPAL_SOURCE_DIR}/rngplay.c
-    ${SDLPAL_SOURCE_DIR}/scene.c
-    ${SDLPAL_SOURCE_DIR}/script.c
-    ${SDLPAL_SOURCE_DIR}/sound.c
-    ${SDLPAL_SOURCE_DIR}/text.c
-    ${SDLPAL_SOURCE_DIR}/ui.c
-    ${SDLPAL_SOURCE_DIR}/uibattle.c
-    ${SDLPAL_SOURCE_DIR}/uigame.c
-    ${SDLPAL_SOURCE_DIR}/util.c
-    ${SDLPAL_SOURCE_DIR}/video.c
-    ${SDLPAL_SOURCE_DIR}/video_glsl.c
-    ${SDLPAL_SOURCE_DIR}/yj1.c
+    ${SDLPAL_DIR}/audio.c
+    ${SDLPAL_DIR}/battle.c
+    ${SDLPAL_DIR}/ending.c
+    ${SDLPAL_DIR}/fight.c
+    ${SDLPAL_DIR}/font.c
+    ${SDLPAL_DIR}/game.c
+    ${SDLPAL_DIR}/global.c
+    ${SDLPAL_DIR}/glslp.c
+    ${SDLPAL_DIR}/input.c
+    ${SDLPAL_DIR}/itemmenu.c
+    ${SDLPAL_DIR}/magicmenu.c
+    ${SDLPAL_DIR}/map.c
+    ${SDLPAL_DIR}/mini_glloader.c
+    ${SDLPAL_DIR}/palcfg.c
+    ${SDLPAL_DIR}/palcommon.c
+    ${SDLPAL_DIR}/palette.c
+    ${SDLPAL_DIR}/play.c
+    ${SDLPAL_DIR}/res.c
+    ${SDLPAL_DIR}/resampler.c
+    ${SDLPAL_DIR}/rixplay.cpp
+    ${SDLPAL_DIR}/rngplay.c
+    ${SDLPAL_DIR}/scene.c
+    ${SDLPAL_DIR}/script.c
+    ${SDLPAL_DIR}/sound.c
+    ${SDLPAL_DIR}/text.c
+    ${SDLPAL_DIR}/ui.c
+    ${SDLPAL_DIR}/uibattle.c
+    ${SDLPAL_DIR}/uigame.c
+    ${SDLPAL_DIR}/util.c
+    ${SDLPAL_DIR}/video.c
+    ${SDLPAL_DIR}/video_glsl.c
+    ${SDLPAL_DIR}/yj1.c
 )
 
 add_library(sdlpal
@@ -42,7 +42,7 @@ target_include_directories(sdlpal
     PRIVATE
         # ${CMAKE_SOURCE_DIR}
         win32
-        ${SDLPAL_INCLUDE_DIR}
+        ${SDLPAL_DIR}
         ${SDL2_SOURCE_DIR}/include
         ${ADPLUG_DIR}
         ${BINIO_INCLUDE_DIR}
