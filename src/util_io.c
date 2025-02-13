b7c60c13417264f4fef5380002c565400d0e6969
Gibobo Chen <gibobo831521.dc@gmail.com>
2025-02-14 00:21:18 +0800

    wip 250214_0

--- src/util_io.c
+++ src/util_io.c
@@ -23,36 +23,36 @@ int UTIL_fseek(void *_Stream, long _Offset, int _Origin) {
    if (_Stream == NULL)
       TerminateOnError("UTIL_fseek() called with invalid parameters\n");
 
-   return fseek(_Stream, _Offset, _Origin);
+   return fseek((FILE *)_Stream, _Offset, _Origin);
 }
 
 unsigned int UTIL_fread(void *_Buffer, unsigned int _ElementSize, unsigned int _ElementCount, void *_Stream) {
    if (_Buffer == NULL || _Stream == NULL)
       TerminateOnError("UTIL_fread() called with invalid parameters\n");
 
-   return (unsigned int)fread(_Buffer, _ElementSize, _ElementCount, _Stream);
+   return (unsigned int)fread(_Buffer, _ElementSize, _ElementCount, (FILE *)_Stream);
 }
 
 unsigned int UTIL_fwrite(void *_Buffer, unsigned int _ElementSize, unsigned int _ElementCount, void *_Stream) {
    if (_Buffer == NULL || _Stream == NULL)
       TerminateOnError("UTIL_fread() called with invalid parameters\n");
 
-   return (unsigned int)fwrite(_Buffer, _ElementSize, _ElementCount, _Stream);
+   return (unsigned int)fwrite(_Buffer, _ElementSize, _ElementCount, (FILE *)_Stream);
 }
 
 void UTIL_fclose(void *fp) {
    if (fp != NULL) {
-      fclose(fp);
+      fclose((FILE *)fp);
    }
 }
 
 long flength(void *fp) {
-   long old_pos = ftell(fp);
+   long old_pos = ftell((FILE *)fp);
    if (old_pos == -1)
       return -1;
    if (UTIL_fseek(fp, 0, SEEK_END) == -1)
       return -1;
-   long length = ftell(fp);
+   long length = ftell((FILE *)fp);
    UTIL_fseek(fp, old_pos, SEEK_SET);
    return length;
 }
